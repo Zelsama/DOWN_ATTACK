ce908a51a2192a677ab9f461e6fcd5cc8a15ed1c
Emerson <emersonlaureanopires@gmail.com>
2025-09-08 18:42:18 -0300

    Resolvendo bug na api

--- BDOOPT_VUE/bdo-optimizer-temp/src/services/api.js
+++ BDOOPT_VUE/bdo-optimizer-temp/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACK_END_URL,
+  baseURL: 'https://' + process.env.VUE_APP_BACK_END_URL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
